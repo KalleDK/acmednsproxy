6850481c29510af88bbe355a8247b7a4c263c08a
Kalle M. Aagaard <git@k-moeller.dk>
2022-02-10 18:08:13 +0100

    workflow

---
+++ .github/actions/alpine/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+export REPODEST="${GITHUB_WORKSPACE}/packages"
+export SRCDEST="${GITHUB_WORKSPACE}/cache/distfiles"
+export PACKAGER_PRIVKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa"
+export PACKAGER_PUBKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
+printf "${INPUT_ABUILD_KEY}" > "${PACKAGER_PRIVKEY}"
+printf "${INPUT_ABUILD_KEY_PUB}" > "${PACKAGER_PUBKEY}"
+cp "${PACKAGER_PUBKEY}" /etc/apk/keys/
+cd ./alpine
+echo "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:11}/"
+sed -i "s/pkgver=.*/pkgver=${INPUT_ABUILD_PKG_VER:11}/" APKBUILD
+abuild -F checksum
+abuild -F -r
+find $REPODEST -name "*.apk" | xargs apk verify
